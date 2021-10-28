6e9a41f494b0c19dee2d3c097ffb2a106eaccc11
Shreyasri6 <90743495+Shreyasri6@users.noreply.github.com>
2021-10-28 22:12:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Form from './components/Form';
+import FormQuery from './components/FormQuery';
+
+function App() {
+  return (
+    <div>
+      <Form/>
+      <FormQuery/>
+      <h1>HIi</h1>
+    </div>
+  )
+}
+
+export default App;
